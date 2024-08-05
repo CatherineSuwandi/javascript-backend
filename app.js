872fb6c26260ab1d465fc229624f663bfa1f5f2a
unknown <suwandicatherine@gmail.com>
2024-08-05 21:04:34 +0700

    commit  pertama

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const http = require('http')
 
 // http.createServer((Request, response)->{
-
+    
 // })
 
+const http = require('http')
 
 http.createServer( function(request, respon) {
     // mendeteksi status http 200 (user berhasil terkoneksi dengan aplikasi kita)
     // Content-type: apa tipe konten yg ingin diberikan ke user
+    // text/plain itu akan menampilkan teks apa adanya
+    // text/html akan merender tag html menjadi tampilan di browser
     respon.writeHead(200, {'Content-type': 'text/plain'})
     // hasil akhir yg akan diberikan ke user
     respon.end('Hello World')
