f271f4b2072d833fe7f2fecf1c38aa5f60f2586c
unknown <suwandicatherine@gmail.com>
2024-08-09 19:44:47 +0700

    basic routing

---
+++ app-express.js
@@ -0,0 +1,23 @@
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+
+app.get('/', function(req, res) {
+    res.send(`<h1>Hello World !!</h1>`)
+})
+
+// the '/hubungi' is called route that exists within the URL
+app.get('/hubungi', function(req, res) {
+    res.send(`<h1>Please Contact: XXX XXX XXXX</h1>`)
+})
+
+app.get('/login', function(req, res) {
+    res.send(`<h1>Nothing here stupid</h1>`)
+})
+
+
+app.listen(port, function() {
+    console.log('Server sudah siap, buka http://localhost:3000' + port)
+
+})
